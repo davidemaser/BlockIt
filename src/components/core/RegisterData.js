ce0c6f8d447993cd73be1f67642c014f62b41e41
David Maser <davidemaser@gmail.com>
2017-08-24 16:22:47 -0400

    Update data register component

--- src/components/core/RegisterData.js
+++ src/components/core/RegisterData.js
@@ -5,11 +5,10 @@ import {Config} from '../../config/Config';
 export default class RegisterData{
   /**
    * This class registers states and state changes and a Config object
-   * Type __faster__ (or the name of the app defined in config/Config)
+   * Type blockIt (or the name of the app defined in config/Config)
    * in console to view the object and it's properties
-   * @param {string} obj
-   * @param {string} val
-   * @param {string=} parent
+   * @param {object[]} data
+   * @param {string=} handle
    */
   constructor(data,handle){
     this.data = data;
